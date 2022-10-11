410745d8e96ac21fb8c5ee17d5781bea4aec23e1
Devika18 <devikankarpe@gmail.com>
2022-10-11 19:33:21 +0530

    first commit

---
+++ src/EmployeeWage/EmployeeWageMain.java
@@ -0,0 +1,13 @@
+package EmployeeWage;
+
+import java.util.Scanner;
+
+public class EmployeeWageMain {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+}
